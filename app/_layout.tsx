f424608288106b354fe53b29534de5906b57a3eb
System <mystrdan@gmail.com>
2026-09-25 10:45:47 +0000

    Add JSI tracking route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,13 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'OPAGH' }} />
-      <Stack.Screen name="request" options={{ title: 'Request delivery' }} />
+      <Stack.Screen name="index" options={{ title: 'JSI' }} />
+      <Stack.Screen name="login" options={{ title: 'Log in' }} />
+      <Stack.Screen name="verify" options={{ title: 'Verify' }} />
+      <Stack.Screen name="request" options={{ title: 'Send or Pick' }} />
       <Stack.Screen name="options" options={{ title: 'Delivery options' }} />
-      <Stack.Screen name="checkout" options={{ title: 'Checkout' }} />
+      <Stack.Screen name="checkout" options={{ title: 'Review request' }} />
+      <Stack.Screen name="tracking" options={{ title: 'Track delivery' }} />
     </Stack>
   );
 }
